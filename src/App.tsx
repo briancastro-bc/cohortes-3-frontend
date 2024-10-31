10a90734c403a28e4093eff62eec9764a3964ba5
briancastro-bc <brian.castro.bedoya@gmail.com>
2024-10-30 20:31:03 -0500

    feat(dinamyc) images fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import {
   FC,
+  useEffect,
 } from 'react';
 import { RouterProvider, } from 'react-router-dom';
 
@@ -8,6 +9,30 @@ import router from './Router';
 type AppProps = object;
 
 const App: FC<AppProps> = () => {
+
+  useEffect(() => {
+
+    const fetchAllImages = async () => {
+      const images = [
+        'react copy',
+        'react',
+      ];
+
+      const allImagesUrls = await Promise.all(
+        images.map(async imageName => {
+          const image = await import(`./assets/${imageName}.svg`);
+          return image.default;
+        })
+      );
+
+      console.log('all images', allImagesUrls);
+    }
+
+    fetchAllImages()
+      .then()
+      .catch(err => console.error(err));
+  }, []);
+
   return (
     <RouterProvider
       router={router}/>
